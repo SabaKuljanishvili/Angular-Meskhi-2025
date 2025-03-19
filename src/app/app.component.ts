603682b39646bdc0e44ee15d725043344fb4d882
saba kuljanishvili <kuljanisaba@gmail.com>
2025-03-19 23:34:18 +0400

    -abourts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
