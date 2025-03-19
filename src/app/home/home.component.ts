0b3139a937896903c85a406f00a87ef0145d85a7
saba kuljanishvili <kuljanisaba@gmail.com>
2025-03-19 23:34:56 +0400

    abouts style + html not working scroll animation --- must add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,131 @@
+// import { CommonModule } from '@angular/common';
+// import { Component, ElementRef, HostListener, ViewChildren, } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [CommonModule, ],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.scss'
+// })
+// export class HomeComponent {
+
+// }
+
+// import { CommonModule } from '@angular/common';
+// import { Component, ElementRef, HostListener, QueryList, ViewChildren, AfterViewInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.scss'
+// })
+// export class HomeComponent implements AfterViewInit {
+//   // Query all elements that should be animated
+//   @ViewChildren('animateOnScroll') animatedElements!: QueryList<ElementRef>;
+  
+//   // Track which elements have already been animated
+//   private animatedIds: Set<number> = new Set();
+  
+//   constructor() {}
+  
+//   ngAfterViewInit() {
+//     // Initial check for elements in viewport on page load
+//     setTimeout(() => {
+//       this.checkElementsInViewport();
+//     }, 100);
+//   }
+  
+//   // Listen for scroll events
+//   @HostListener('window:scroll', ['$event'])
+//   onScroll() {
+//     this.checkElementsInViewport();
+//   }
+  
+//   // Check if elements are in viewport and should be animated
+//   private checkElementsInViewport() {
+//     if (!this.animatedElements) return;
+    
+//     this.animatedElements.forEach((element, index) => {
+//       // Skip already animated elements
+//       if (this.animatedIds.has(index)) return;
+      
+//       const elementPosition = element.nativeElement.getBoundingClientRect();
+//       const windowHeight = window.innerHeight;
+      
+//       // If element is in viewport (with some offset for better effect)
+//       if (elementPosition.top < windowHeight - 100) {
+//         // Add the visible class to trigger animation
+//         element.nativeElement.classList.add('visible');
+//         // Mark this element as already animated
+//         this.animatedIds.add(index);
+//       }
+//     });
+//   }
+// }
+
+
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, ViewChild, } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { Component, ElementRef, HostListener, QueryList, ViewChildren, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule,],
+  imports: [CommonModule, ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  @ViewChild('videoPlayer') videoElement!: ElementRef<HTMLVideoElement>;
+export class HomeComponent implements AfterViewInit {
+  // Query all elements that should be animated
+  @ViewChildren('animateOnScroll') animatedElements!: QueryList<ElementRef>;
   
-  public showPlayButton = false;
-
-  ngAfterViewInit(): void {
-
-    if (this.videoElement && this.videoElement.nativeElement) {
-
-      const video = this.videoElement.nativeElement;
-      video.autoplay = true;
-      video.muted = true;
-      video.loop = false;
-      video.controls = false;
-      
-      let playPromise = video.play();
-      
+  // Track which elements have already been animated
+  private animatedIds: Set<number> = new Set();
+  
+  // Flag to indicate if JS is running
+  isJsWorking = false;
+  
+  constructor() {}
+  
+  ngAfterViewInit() {
+    // Set JS working flag to true
+    this.isJsWorking = true;
     
-    }
+    // Apply the js-working class to the body to trigger CSS
+    document.body.classList.add('js-working');
+    
+    // Initial check for elements in viewport on page load
+    setTimeout(() => {
+      this.checkElementsInViewport();
+    }, 100);
   }
-
-
-
-
-
+  
+  // Listen for scroll events
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.checkElementsInViewport();
+  }
+  
+  // Check if elements are in viewport and should be animated
+  private checkElementsInViewport() {
+    if (!this.animatedElements) return;
+    
+    this.animatedElements.forEach((element, index) => {
+      // Skip already animated elements
+      if (this.animatedIds.has(index)) return;
+      
+      const elementPosition = element.nativeElement.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      
+      // If element is in viewport (with some offset for better effect)
+      if (elementPosition.top < windowHeight - 100) {
+        // Add the visible class to trigger animation
+        element.nativeElement.classList.add('visible');
+        // Mark this element as already animated
+        this.animatedIds.add(index);
+      }
+    });
+  }
 }
