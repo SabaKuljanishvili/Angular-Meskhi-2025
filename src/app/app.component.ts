20fd7c46712ce3a665126d3ab43a321634fa5798
saba kuljanishvili <kuljanisaba@gmail.com>
2025-03-29 19:25:14 +0400

    dkd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { FooterComponent } from "./footer/footer.component";
