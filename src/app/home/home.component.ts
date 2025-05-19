a31b740b5cc434c564c2f995d9a724889360573f
saba kuljanishvili <kuljanisaba@gmail.com>
2025-05-19 23:29:05 +0400

    add new style need some fix and better color on bg and - about and move to about

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,34 +4,10 @@ import { Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-  @ViewChildren('scrollAnimate') animatedElements!: QueryList<ElementRef>;
-
-  ngAfterViewInit(): void {
-    this.onScroll(); 
-  }
-
-  @HostListener('window:scroll', [])
-  onScroll(): void {
-    const windowHeight = window.innerHeight;
-
-    this.animatedElements.forEach((elementRef: ElementRef) => {
-      const element = elementRef.nativeElement;
-      const position = element.getBoundingClientRect().top;
-
-      if (position < windowHeight - 100) {
-        element.classList.add('visible');
-      } else {
-        element.classList.remove('visible');
-      }
-    });
-  }
-
-  
 }
 
