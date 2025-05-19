a55af9044606a66d4435abf715362c19cc91add2
saba kuljanishvili <kuljanisaba@gmail.com>
2025-05-19 23:27:40 +0400

    add route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+     activeRouteClass = "active"
+
+
+     isMenuOpen = false;
+   
+     toggleMenu(): void {
+       this.isMenuOpen = !this.isMenuOpen;
+     }
+   
+     closeMenu(): void {
+       this.isMenuOpen = false;
+     }
+     @HostListener('window:resize', ['$event'])
+     onResize(event: Event): void {
+       const window = event.target as Window;
+       if (window.innerWidth > 768) {
+         this.closeMenu();
+       }
+     }
+     isDarkTheme = false;
+
 }
