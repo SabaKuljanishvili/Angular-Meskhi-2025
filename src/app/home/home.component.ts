c26f41a460fec209f0c4fc4686f826115caa2e32
saba kuljanishvili <kuljanisaba@gmail.com>
2025-08-22 22:43:23 +0400

    new better style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,43 +1,25 @@
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
-import { HorizontalSliderComponent } from "../horizontal-slider/horizontal-slider.component";
-import { register } from 'swiper/element/bundle';
-register();
+import { NavigationComponent } from '../navigation/navigation.component';
+import { HeroComponent } from '../hero/hero.component';
+import { ServicesComponent } from '../services/services.component';
+import { GalleryComponent } from '../gallery/gallery.component';
+import { ContactSectionComponent } from '../contact-section/contact-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HorizontalSliderComponent, ],
+  imports: [
+    CommonModule,
+    NavigationComponent,
+    HeroComponent,
+    ServicesComponent,
+    GalleryComponent,
+    ContactSectionComponent
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  encapsulation: ViewEncapsulation.None,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-    firstSwiperImages = [
-    'https://picsum.photos/id/1001/600/400',
-    'https://picsum.photos/id/1002/600/400',
-    'https://picsum.photos/id/1003/600/400'
-  ];
-
-  secondSwiperImages = [
-    'https://picsum.photos/id/1011/600/400',
-    'https://picsum.photos/id/1012/600/400',
-    'https://picsum.photos/id/1013/600/400'
-  ];
-
-  thirdSwiperImages = [
-    'https://picsum.photos/id/1021/600/400',
-    'https://picsum.photos/id/1022/600/400',
-    'https://picsum.photos/id/1023/600/400'
-  ];
-
-    fourSwiperImages = [
-    'https://picsum.photos/id/1011/600/400',
-    'https://picsum.photos/id/1012/600/400',
-    'https://picsum.photos/id/1013/600/400'
-  ];
-
-
-  
+  // Component is now just a container for other components
 }
