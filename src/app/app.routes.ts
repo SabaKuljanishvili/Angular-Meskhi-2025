697fce5c5cfbda24ec18ea99c8e7800dc059df2d
saba kuljanishvili <kuljanisaba@gmail.com>
2025-08-24 23:09:37 +0400

    - contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
      {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component:HomeComponent},
-    {path: 'contact', component:ContactComponent},
     {path: 'about', component:AboutComponent},
     {path: '**', component:ErrorComponent},
 
@@ -16,11 +14,6 @@ export const routes: Routes = [
         path: 'home',
         title: 'home',
         loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
-    },
-        {
-        path: 'contact',
-        title: 'contact',
-        loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
     },
             {
         path: 'about',
